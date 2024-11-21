a183781faa5dc627c79dfd7406a041581f4e47c9
Hakeem <hazaak009@gmail.com>
2024-11-22 00:11:03 +0100

    yes

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function page() {
   return (
     <section className="py-24">
       <div className='container'>
-        <h1 className='text-3xl font-bold'>Shambolizing</h1>
+        <h1 className='text-3xl font-bold'>Shambolizing Why aa you gae</h1>
       </div>
     </section>
   )
